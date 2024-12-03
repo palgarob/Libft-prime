19c7c322b1d42c4901b5bb55aa1977fdbde849c2
Pedro Palomares <pedpalgarob@gmail.com>
2024-12-03 23:33:23 +0100

    progress

--- src/mat3x3.c
+++ src/mat3x3.c
@@ -6,7 +6,7 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 18:02:46 by pepaloma          #+#    #+#             */
-/*   Updated: 2024/12/03 18:19:06 by pepaloma         ###   ########.fr       */
+/*   Updated: 2024/12/03 18:28:07 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 t_vec3	mat3x3xvec3(t_mat3x3 mat, t_vec3 vec)
 {
-	t_vec3	output;
-
-	output = vec_add(
+	return (vec_add(
 			vec_scale_m(mat.i, vec.x),
 			vec_add(
 				vec_scale_m(mat.j, vec.y),
 				vec_scale_m(mat.k, vec.z)
 			)
-	);
+	));
 }
